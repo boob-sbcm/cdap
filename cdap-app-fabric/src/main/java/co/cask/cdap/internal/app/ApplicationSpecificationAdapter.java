293da0df5dae673850c3626bad1c10af5f927fe4
Terence Yim <terencey@google.com>
2018-11-28 16:34:33 -0800

    (CDAP-13135) Remove some usages of the deprecated InputSupplier and OutputSupplier (WIP)

--- cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/ApplicationSpecificationAdapter.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/ApplicationSpecificationAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014-2016 Cask Data, Inc.
+ * Copyright © 2014-2018 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -51,7 +51,6 @@ import co.cask.cdap.proto.codec.WorkflowSpecificationCodec;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
 import com.google.common.collect.Maps;
-import com.google.common.io.InputSupplier;
 import com.google.common.io.OutputSupplier;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -135,13 +134,6 @@ public final class ApplicationSpecificationAdapter {
     }
   }
 
-  public void toJson(ApplicationSpecification appSpec,
-                     OutputSupplier<? extends Writer> outputSupplier) throws IOException {
-    try (Writer writer = outputSupplier.getOutput()) {
-      toJson(appSpec, writer);
-    }
-  }
-
   public ApplicationSpecification fromJson(String json) {
     return gson.fromJson(json, ApplicationSpecification.class);
   }
@@ -151,12 +143,6 @@ public final class ApplicationSpecificationAdapter {
       return gson.fromJson(reader, ApplicationSpecification.class);
     } catch (JsonParseException e) {
       throw new IOException(e);
-    }
-  }
-
-  public ApplicationSpecification fromJson(InputSupplier<? extends Reader> inputSupplier) throws IOException {
-    try (Reader reader = inputSupplier.getInput()) {
-      return fromJson(reader);
     }
   }
 
